777cfc4badd1af434a5bb309f0eadd8dc7a61302
MacBook <macbook@Air-MacBook.bbrouter>
2022-08-20 14:30:46 -0700

    exercise

--- task-2/main.js
+++ task-2/main.js
@@ -4,6 +4,13 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
-  
+   let n = result.input;
+   let a = parseInt(n.split(",")[0]);
+   let b =  parseInt(n.split(",")[1]);
+   let c = parseInt(n.split(",")[2]);
+   if (a > b && b < c) {
+    console.log("IN");
+   }else{
+    console.log("OUT");
+   }
 });
