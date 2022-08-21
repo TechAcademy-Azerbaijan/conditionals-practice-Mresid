61cfd5136ec46a36304426bdace05e1c9aabe983
MacBook <macbook@Air-MacBook.bbrouter>
2022-08-21 03:06:09 -0700

    exercise

--- task-5/main.js
+++ task-5/main.js
@@ -3,7 +3,18 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+
+   let n = result.input;//1378
+   let b =parseInt(n / 100);//13
+   let c = parseInt(n % 1000);//378
+   let d =parseInt(c / 10);//137
+   let e =parseInt(n % 100)//78
+   if (b == 37 || d == 37 || e == 37) {
+      console.log("YES");
+   }else{
+      console.log("NO");
+   }
+   
+   
+
 });
